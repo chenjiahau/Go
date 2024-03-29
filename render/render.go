cc87e6878a0218f3b6966059379d8737f03de688
chenjiahau <chenjiahau@gmail.com>
2024-03-29 13:53:58 +0800

    Cache templates

--- render/render.go
+++ render/render.go
@@ -3,14 +3,47 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
-func Render(w http.ResponseWriter, tpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tpl, "./templates/layout.html")
-	err := parsedTemplate.Execute(w, nil)
+var templateCache = make(map[string]*template.Template)
 
+func createTemplateCache(t string) error {
+	templates := []string {
+		fmt.Sprintf("./templates/%s", t), "./templates/layout.html",
+	}
+
+	tpl, err := template.ParseFiles(templates...)
 	if err != nil {
-		fmt.Println("error parsing template:", err)
+		return err
+	}
+
+	templateCache[t] = tpl
+
+	return nil
+}
+
+func Render(w http.ResponseWriter, t string) {
+	var tpl *template.Template
+	var err error
+
+	_, inMap := templateCache[t]
+	if !inMap {
+		log.Printf("Creating template from cache: %s\n", t)
+		err = createTemplateCache(t)
+
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		log.Printf("Using template from cache: %s\n", t)
+	}
+
+	tpl = templateCache[t]
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
 	}
 }
