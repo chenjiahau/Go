e2a4028b3856c99257029baf41350159f2b7f3f7
ivanchen <ivan.chen@dlinkcorp.com>
2024-04-20 07:59:26 +0800

    Initialization

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-const portNumber = ":8080"
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
-}
-
 func main() {
-	http.HandleFunc("/", Home)
-
-	log.Println("Starting application on port", portNumber)
-	_ = http.ListenAndServe(portNumber, nil)
+	
 }
