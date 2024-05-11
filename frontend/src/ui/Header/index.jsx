53f55a13132745268efe436553d1e59071def4ba
ivanchen <ivan.chen@dlinkcorp.com>
2024-05-11 20:50:17 +0800

    Clicking logout have to send the sign-out request.

--- frontend/src/ui/Header/index.jsx
+++ frontend/src/ui/Header/index.jsx
@@ -3,7 +3,8 @@ import Logo from "@/assets/img/brand.png";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 
-import messageUtil from "../../util/message.util";
+import apiHandler from "@/util/api.util";
+import messageUtil, { commonMessage } from "../../util/message.util";
 
 const message = {
   LOGOUT_SUCCESS: "Logout successfully",
@@ -13,10 +14,15 @@ const Header = (props) => {
   const { user, onCleanUser } = props;
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    onCleanUser();
-    navigate("/");
-    messageUtil.showSuccessMessage(message.LOGOUT_SUCCESS);
+  const handleLogout = async () => {
+    try {
+      await apiHandler.get("/auth/sign-out");
+      onCleanUser();
+      navigate("/");
+      messageUtil.showSuccessMessage(message.LOGOUT_SUCCESS);
+    } catch (error) {
+      messageUtil.showErrorMessage(commonMessage.error);
+    }
   };
 
   return (
