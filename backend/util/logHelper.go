28b4e76ba27a7f2c39f1af61c12e01bf349636af
ivanchen <ivan.chen@dlinkcorp.com>
2024-06-05 14:09:01 +0800

    Making the out of log has color level.

--- backend/util/logHelper.go
+++ backend/util/logHelper.go
@@ -1,34 +1,66 @@
 package util
 
 import (
+	"fmt"
+
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
+
+var ZapLog *zap.Logger
+
+func init() {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey: "time",
+		LevelKey: "level",
+		NameKey: "logger",
+		CallerKey: "caller",
+		MessageKey: "msg",
+		StacktraceKey: "stacktrace",
+		LineEnding: zapcore.DefaultLineEnding,
+		EncodeLevel: zapcore.CapitalColorLevelEncoder,
+		EncodeTime: zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller: zapcore.FullCallerEncoder,
+	}
+
+	atom := zap.NewAtomicLevelAt(zap.DebugLevel)
+	config := zap.Config{
+		 Level: atom,
+		 Development: true,
+		//  Encoding: "json",
+		 Encoding: "console",
+		 EncoderConfig: encoderConfig,
+		 OutputPaths: []string{"stdout",},
+		 ErrorOutputPaths: []string{"stderr"},
+	}
+
+	config.EncoderConfig.EncodeLevel = zapcore.LowercaseColorLevelEncoder
+
+	var err error
+	ZapLog, err = config.Build()
+	if err != nil {
+		panic(fmt.Sprintf("can't initialize zap logger: %v", err))
+	}
+}
 
 func WriteInfoLog(message string) {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 
-	logger.Info(message)
+	ZapLog.Info(message)
 }
 
 func WriteWarnLog(message string) {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 
-	logger.Warn(message)
+	ZapLog.Warn(message)
 }
 
 func WriteErrorLog(message string) {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 
-	logger.Error(message)
+	ZapLog.Error(message)
 }
-
-func WriteDebugLog(message string) {
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
-
-	logger.Debug(message)
-}
-
